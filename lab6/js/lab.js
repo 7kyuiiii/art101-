b146ba86c5371edda3c6a0654da0dc5d56fc1eab
7kyuiiii <fanyuqi00@gmail.com>
2024-10-28 12:59:14 -0700

    Changed

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
 
-// Constants
+ //Author:    Yuqi Fan
+ // Created:   2024.10.23
+ 
+// Define Variables
+myTransport =["bus", "subway","bike", "legs"];
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Create an object for my main ride
+myMainRide= {
+  make: "Ford",
+  model: "Taurus",
+  color: "Red",
+  year: 2003,
+  //we can define a function within our object (called a method)
+  // that uses the value of year above (using this.year)
+  age :function () {
+    return 2024 - this.year;
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// output
+document.writeln("Kinds of transportation I use", myTransport, "</br>");
+// this little trick allows us to write an object to the document
+document.writeln("My main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'),"</pre>");
 
-// let's get this party started
-main();
