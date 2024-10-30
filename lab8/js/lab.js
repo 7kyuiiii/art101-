56de80ab0c518e8a0f15157e21836cec92a86ec1
7kyuiiii <fanyuqi00@gmail.com>
2024-10-30 11:19:10 -0700

    Changed

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -8,14 +8,14 @@ function isEven(x){
     return (x % 2 == 0);
 }
 
-//test function
-console.log("Is 1 Even?", isEven(1));
-console.log("Is 2 Even?", isEven(2));
+// test function
+console.log("Is 1 even? ", isEven(1));
+console.log("Is 2 even? ", isEven(2));
 
 array = [100,7,25,8,24,145,10000]
 console.log("My array", array);
 
-var result = array.map (isEven);
+var result = array.map(isEven);
 // should return [true, false, false, true, true, false, true]
 console.log("Test of evenness of array:", result);
 
@@ -23,4 +23,4 @@ var result = array.map(function(x){
     return x ** 1.5;
 })
 // should return [1000, 18.520259177452132, 125, 22.62741699796952, 117.57550765359255, 1746.0312139248829, 1000000]
-console.log("Squareroot of array", result);
+console.log("Squareroot of array:", result);
